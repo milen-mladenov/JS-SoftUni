f6ecfe2a12f32f322c9efdecddb0aceb9b87a589
Milen_Mladenov <milen_s_mladenov@yahoo.co.uk>
2022-04-08 18:15:47 +0300

    Add files via upload

---
+++ 00. Basics/01. First Steps In Coding/01. First Steps In Coding/Exercise/USDtoBGN.js
@@ -0,0 +1,10 @@
+function exchange(index){
+
+    let usd = Number(index[0]);
+    let bgn = usd * 1.79549;
+
+    console.log(bgn);
+
+}
+
+exchange (["100"])
---
+++ 00. Basics/01. First Steps In Coding/01. First Steps In Coding/Intro/squareArea.js
@@ -0,0 +1,5 @@
+function squareArea(input) {
+
+    let a = Number(input[0]);
+    console.log(a * a);
+}
